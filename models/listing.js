cee09803d2fa368f7f5bbdf59e785dce763c0523
Likon Barman <mrlikhonbarman@gmail.com>
2025-04-14 23:05:21 +0530

    No changed

--- models/listing.js
+++ models/listing.js
@@ -1,25 +1,29 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-
 const listingSchema = new Schema({
-    title:{
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+//   image:String,
+  image: {
+    filename: String,
+    url: String
+  },
+//   image2: {
+//     type: String,
+//     default:
+//       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//     set: (v) =>
+//       v === ""
+//         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+//         : v,
+//   },
+  price: Number,
+  location: String,
+  country: String,
+});
 
-        type : String,
-        required :true
-    } ,
-    description:String,
-    image:{ 
-        type:String,
-        default : "https://unsplash.com/photos/train-tracks-lead-to-a-view-of-mount-fuji-PIJel1qTrOY",
-        Set :(v) => 
-            v === "" ? "https://unsplash.com/photos/train-tracks-lead-to-a-view-of-mount-fuji-PIJel1qTrOY"
-            :v,
-    },
-    price:Number,
-    location:String,
-    country : String
-})
-
-const Listing = mongoose.model("listing", listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
